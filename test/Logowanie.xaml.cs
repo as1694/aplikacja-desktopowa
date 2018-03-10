90dcf5f940f24b077df805d7c846731d5f1a2f32
as1694 <32881486+as1694@users.noreply.github.com>
2018-03-10 11:44:25 +0100

    first commit

---
+++ test/Logowanie.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test
+{
+    /// <summary>
+    /// Interaction logic for Logowanie.xaml
+    /// </summary>
+    public partial class Logowanie : Window
+    {
+        public Logowanie()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonZaloguj_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextLogin.Text == "admin" && Password.Password == "admin")
+            {
+                (new Main()).Show();
+
+                Close();
+            }
+            else
+                KomH.Content = "Nieprawidłowy login lub hasło.";
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                buttonZaloguj_Click(sender, e);
+            }
+        }
+
+        private void buttonPubliczny_Click(object sender, RoutedEventArgs e)
+        {
+            (new Main()).Show();
+
+            Close();
+        }
+
+        private void buttonNowyProfil_Click(object sender, RoutedEventArgs e)
+        {
+            (new NowyProfil()).Show();
+
+            Close();
+        }
+    }
+}
---
+++ test/NowyProfil.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace test
+{
+    /// <summary>
+    /// Interaction logic for NowyProfil.xaml
+    /// </summary>
+    public partial class NowyProfil : Window
+    {
+        public NowyProfil()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStworzKonto_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextNLogin.Text != "" && NewPassword.Password == NewPassword2.Password)
+            {
+                (new Main()).Show();
+
+                Close();
+            }
+            else
+            {
+                if(TextNLogin.Text == "" && NewPassword != null && NewPassword.Password != NewPassword2.Password)
+                    KomNP.Content = "Podana nazwa użytkownika jest już zajęta. Hasła są różne";
+                if (TextNLogin.Text != null && NewPassword.Password == NewPassword2.Password)
+                    KomNP.Content = "Podana nazwa użytkownika jest już zajęta.";
+                else
+                    KomNP.Content = "Hasła są różne";
+            }
+        }
+    }
+}
